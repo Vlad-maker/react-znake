a49c1b761b563dfa76d2d6467dc0233085eb2e7b
Vlad-maker <skateislife@mail.ru>
2021-10-06 19:08:14 +0300

    Feat:some logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BOARD_SIZE = 10;
+const DEFAULT_CELLS_VALUE = Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill(0));
+const AVAILABLE_MOVES =['ArrowDown', 'ArrowUp', 'ArrowRighr', 'ArrowLeft'];
+const SPEED = 500
+
+const App = () => {
+
+    const [direction, setDirection] = React.useState(AVAILABLE_MOVES[0]);
+    const [snake, setSnake] = React.useState([[1, 1]])
+
+    const handleKeyDown = (event) => {
+      const index = AVAILABLE_MOVES.indexOf(event.key)
+      if ( index > -1) {
+        setDirection(AVAILABLE_MOVES[index])
+      }
+    }
+
+    React.useEffect(() => {
+      document.addEventListener('keydown', handleKeyDown)
+    })
+
+    return (
+        <div>
+          {DEFAULT_CELLS_VALUE.map((row, indexR) => (
+            <div key={indexR} className='row'>
+              {row.map((cell, indexC) => (
+                let type = snake.some(elem => elem[0] === indexR && elem[1] === indexC) && 'snake';
+                return (<div className={`cell ${type}`}/>)
+              ))}
+            </div>
+          ))}
+            
+            <div className='cell snake'/>
+            <div className='cell food'/>
+            <div className={`arrow down ${direction === 'ArrowDown' ? 'selected' : ''}`}/>
+            <div className={`arrow up ${direction === 'ArrowUp' ? 'selected' : ''}`}/>
+            <div className={`arrow left ${direction === 'ArrowLeft' ? 'selected' : ''}`}/>
+            <div className={`arrow right ${direction === 'ArrowRight' ? 'selected' : ''}`}/>
+        </div>
+    );
 }
+
 
 export default App;
